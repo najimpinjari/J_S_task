ec91404ff48700ad4bfad69ae728baf80b3b5ff1
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-01-24 18:59:30 +0530

    Add files via upload

---
+++ Array_task.js
@@ -0,0 +1,25 @@
+//empty array
+
+let emptyarray =[];
+
+// array with values 
+let fruits =["aaple" , "banana", "orange", "grafh"];
+
+// for (let i = 0; i<fruits.length; i++){
+//     console.log(fruits[1]);
+// }
+// fruits[1] = "mango"; //for Array update 
+
+// fruits.push("potato"); //for add last index numer item adding function 
+
+// fruits.pop(); // for last item of index should be remove 
+
+// fruits.shift(); //remove the first element of an array 
+
+// fruits.forEach(function(fruits)
+// {
+//     console.log(fruits)
+// });
+fruits.unshift("stobary");
+
+console.log(fruits);
