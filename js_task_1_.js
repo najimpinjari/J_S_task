e7ff6fcf81deb213879e5c21a5f2f793cf1f30ea
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-01-24 13:13:44 +0530

    Add files via upload

---
+++ js_task_1_.js
@@ -0,0 +1,53 @@
+// alert("my name is bob ");
+// const student ={
+//     fullname : "Rupesh raut",
+//     age : 20,
+//     cgpa : 8.2,
+//     ispass : true
+// };
+// console.log(student.age);
+// console.log(student["fullname"]);
+
+// let name = prompt("what is your name ");
+
+// console.log(name);
+
+// let num = prompt("Enter your nameber");
+//     if(num % 5 === 0){
+//         console.log(num ,"num is multiple of 5")
+//     }else{
+//         console.log(num ,"num is not multiple of 5")
+//     }
+// let score = prompt("enter your score , we found the grades");
+// let grades;
+// if(score >= 90 && score <= 100)
+// {
+//     grades = "A" ;
+// }
+// else if (score >= 70 && score <= 89)
+// {
+//     grades = "B" ;
+
+// }
+// else if (score >= 60 && score <= 69)
+// {
+//     grades = "C" ;
+// }
+// else if (score >= 50 && score <= 59)
+// {
+//     grades = "D" ;
+
+// }
+// else if (score >= 0 && score <= 49)
+// {
+//     grades = "F" ;
+// } 
+
+// console.log("according to your score your grades is : " , grades);
+
+//for of for
+let name = "najimdp";
+
+for( let i of name){
+    console.log(i);
+}
