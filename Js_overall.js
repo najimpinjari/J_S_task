de462abf4d8214824084d7a07f8a2532317aa4bc
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-02-07 19:21:30 +0530

    Add files via upload

---
+++ Js_overall.js
@@ -0,0 +1,24 @@
+importtant function of js inbuild 
+
+alert("hello mamma");
+
+console.log("hello mamma ");
+
+document.write("hello mamma ");
+
+prompt("you can do it aggre ");
+
+variables
+
+var name;
+name = "najim dp ";
+name = "rupesh raut"; // variable override one variable have many name but output of the variable its the last value
+alert(name);
+
+document.getElementById("one").innerHTML = "welcome to my world ";
+document.write(x);
+console.log(x);
+
+var c = document.getElementsByClassName("class");
+console.log(c);
+document.write(c);
